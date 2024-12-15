a4da4fdbde0ee7b5ffa8daae9b9bed38aba58d3e
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2024-12-15 09:14:13 -0300

    Update mysql_install_1.sh

--- script x64/dockersh/mysql_install_1.sh
+++ script x64/dockersh/mysql_install_1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Pedir la ruta de instalación
-read -p "Ingrese el path de instalación para el contenedor Docker: " DOCKER_PATH
+# Solicitar la entrada del usuario para el path de instalación
+read -p "Ingrese el path de instalación para el contenedor Docker: " path
 
-# Pedir el nombre del contenedor
-read -p "Ingrese un nombre para el contenedor Docker: " CONTAINER_NAME
+# Solicitar la entrada del usuario para el nombre del contenedor
+read -p "Ingrese un nombre para el contenedor Docker: " nombre
 
-# Pedir la contraseña del usuario 'sa'
-read -sp "Ingrese la contraseña para el usuario 'sa': " SA_PASSWORD
-echo
-
-# Crear el contenedor Docker de SQL Server
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$SA_PASSWORD" -p 1433:1433 --name $CONTAINER_NAME --hostname $CONTAINER_NAME -d mcr.microsoft.com/mssql/server:2022-latest
+# Contenedor Docker
+docker run -d \
+  --name $nombre \
+  -v $path:/var/lib/mysql \
+  -e MYSQL_ROOT_PASSWORD=root \
+  mysql:latest
