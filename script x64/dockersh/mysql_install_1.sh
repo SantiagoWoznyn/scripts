fdb41a4bd312889592e618a41cc98b12fe754796
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2024-12-15 09:46:55 -0300

    Update mysql_install_1.sh

--- script x64/dockersh/mysql_install_1.sh
+++ script x64/dockersh/mysql_install_1.sh
@@ -6,9 +6,17 @@ read -p "Ingrese el path de instalación para el contenedor Docker: " path
 # Solicitar la entrada del usuario para el nombre del contenedor
 read -p "Ingrese un nombre para el contenedor Docker: " nombre
 
-# Contenedor Docker
-docker run -d \
-  --name $nombre \
-  -v $path:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=root \
-  mysql:latest
+# Solicitar la entrada del usuario para la contraseña de SA de MSSQL
+read -p "Ingrese una contraseña para SA de MSSQL: " MSSQL_SA_PASSWORD
+
+# Solicitar la entrada del usuario para el puerto
+read -p "Ingrese el número de puerto para el contenedor Docker: " puerto
+
+# Docker run para iniciar un contenedor de Microsoft SQL Server
+docker run -e "ACCEPT_EULA=Y" \
+           -e "MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD" \
+           -e "MSSQL_PID=Evaluation" \
+           -p $puerto:1433 \
+           --name $nombre \
+           --hostname $nombre \
+           -d mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
