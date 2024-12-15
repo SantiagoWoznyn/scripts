8dc296b21e7ca8158fdcdc1e12c423587bd599f5
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2024-12-15 08:37:17 -0300

    Update mysql_install_1.sh

--- script x64/dockersh/mysql_install_1.sh
+++ script x64/dockersh/mysql_install_1.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Solicitar el directorio para el volumen SQL Server
-read -p "Ingrese el directorio donde desea almacenar los datos del servidor SQL (por ejemplo, /opt/sql-server-data): " sql_dir
-mkdir -p "$sql_dir"
+# Solicitar el directorio para los datos de MySQL
+read -p "Ingrese el directorio donde desea almacenar los datos de MySQL (por ejemplo, /opt/mysql-data): " mysql_dir
+mkdir -p "$mysql_dir"
 
-# Solicitar un nombre de usuario y contraseña para el servidor SQL
-read -p "Ingrese el nombre de usuario para el servidor SQL: " sql_user
-read -sp "Ingrese la contraseña para el servidor SQL: " sql_password
-echo
+# Crear el archivo .env para las variables de entorno
+cat <<EOL > sqlserver.env
+MYSQL_ROOT_PASSWORD=tu_contraseña
+MYSQL_DATABASE=mi_base_datos
+MYSQL_USER=mi_usuario
+MYSQL_PASSWORD=mi_contraseña
+EOL
 
 # Crear el archivo docker-compose.yml
 cat <<EOL > docker-compose.yml
@@ -18,32 +21,25 @@ networks:
     driver: bridge
 
 volumes:
-  sql-server-data:
+  mysql-data:
     driver: local
-  sqldata:
-  sqllog:
-  sqlbackup:
 
 services:
-  db:
-    image: mcr.microsoft.com/mssql/server:2019-CU3-ubuntu-18.04
-    container_name: db-sqlserver
+  mysql:
+    image: mysql:latest
+    container_name: mysql-server
     networks:
       - app-network-public
     restart: always
     environment:
-      SA_PASSWORD: "$sql_password"
-      ACCEPT_EULA: "Y"
-    env_file:
-      - sqlserver.env
-      - sapassword.env
+      MYSQL_ROOT_PASSWORD: "tu_contraseña"
+      MYSQL_DATABASE: "mi_base_datos"
+      MYSQL_USER: "mi_usuario"
+      MYSQL_PASSWORD: "mi_contraseña"
     ports:
-      - '1433:1433'
+      - '3306:3306'
     volumes:
-      - sql-server-data:/var/opt/mssql/
-      - sqldata:/var/opt/sqlserver/data
-      - sqllog:/var/opt/sqlserver/log
-      - sqlbackup:/var/opt/sqlserver/backup
+      - mysql-data:/var/lib/mysql
       - /c/docker/shared:/usr/shared
 EOL
 
