9c81f0eec86a8e4126d32780624ef5480bb6f16f
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2025-01-10 06:14:15 -0300

    Update nginx_server.sh

--- script rpi4/dockersc/nginx_server.sh
+++ script rpi4/dockersc/nginx_server.sh
@@ -5,7 +5,10 @@ read -p "Introduce la ruta donde se encuentra el código de la API (./api): " ap
 read -p "Introduce la ruta donde se encuentra el contenido de html5up-stellar (./html5up-stellar): " html_path
 read -p "Introduce la ruta donde se encuentra el archivo de configuración de Nginx (./nginx/default.conf): " nginx_conf_path
 
-# Crear un archivo docker-compose.yml con las rutas proporcionadas
+# Preguntar al usuario por el puerto para Nginx
+read -p "Introduce el puerto donde se expondrá Nginx (por ejemplo, 8089): " nginx_port
+
+# Crear un archivo docker-compose.yml con las rutas proporcionadas y el puerto
 cat > docker-compose.yml <<EOL
 version: "3.8"
 services:
@@ -19,7 +22,7 @@ services:
     restart: always
     image: nginx:1-alpine
     ports:
-      - 8089:80
+      - ${nginx_port}:80
     volumes:
       - ${html_path}:/var/www/html
       - ${nginx_conf_path}:/etc/nginx/conf.d/default.conf
@@ -27,10 +30,11 @@ services:
       - node
 EOL
 
-echo "docker-compose.yml ha sido generado con las rutas proporcionadas."
+echo "docker-compose.yml ha sido generado con las rutas y el puerto proporcionados."
 
 # Levantar los contenedores de Docker
 echo "Levantando los contenedores de Docker..."
 sudo docker-compose up -d
 
-echo "Los contenedores han sido levantados."
+echo "El contenedor ha sido levantado."
+
