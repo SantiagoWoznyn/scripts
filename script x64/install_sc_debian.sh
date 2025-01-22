f56cbc67536c4dbae1e4559efbecf8cd9101935a
SantiagoWoznyn <135929395+SantiagoWoznyn@users.noreply.github.com>
2025-01-22 15:29:09 -0300

    Update install_sc_debian.sh

--- script x64/install_sc_debian.sh
+++ script x64/install_sc_debian.sh
@@ -4,9 +4,6 @@
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
 
-# Actualizar los paquetes existentes
-sudo apt update
-sudo apt upgrade -y
 
 # Instalar paquetes necesarios
 sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
